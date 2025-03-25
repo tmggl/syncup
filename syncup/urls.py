6edf93f5b61fea2e81d978497f67f42cbbc738c3
tmggl <tmggl60@gmail.com>
2025-03-25 16:47:16 +0300

    58

--- syncup/urls.py
+++ syncup/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.static import serve
-from django.urls import re_path
-from users.views import home_view  # تأكد من استيراد `home_view`
 from django.conf import settings
 from django.conf.urls.static import static
+from users.views import home_view  # تأكد من استيراد `home_view`
 
 urlpatterns = [
     path('admin/', admin.site.urls),  # لوحة تحكم Django
@@ -17,10 +16,12 @@ urlpatterns = [
     path('chat/', include('chat.urls')),  # روابط الدردشة
 ]
 
-# 📂 دعم تقديم ملفات `media/` في وضع التطوير والإنتاج
+# 📂 تقديم ملفات media و static في بيئة التطوير أو الإنتاج
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 else:
     urlpatterns += [
         re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     ]
